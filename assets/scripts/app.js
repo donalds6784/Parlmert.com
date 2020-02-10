7ddc39accb9d80acdd906a336ef72774ae8228f8
Donalds6784 <donalds6784@gmail.com>
2020-02-10 13:04:21 +0300

    Nav menu is now collapsable

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 'use strict';
+// BEGINNING: Sticky Nav 
 let mainNav = document.querySelector('.nav-header');
 
 window.onscroll = function () {
@@ -10,7 +11,25 @@ window.onscroll = function () {
         mainNav.classList.remove('sticky-nav');
     }       
 };
+// END: Sticky nav
 
+// BEGINNING: Nav Toggle Button
+let responsiveNav, nav;
+responsiveNav = document.querySelector('.nav-toggle-btn');
+nav = document.querySelector('.nav-modifier');
+let navIsOpen = false;
 
-let nav;
+responsiveNav.addEventListener('click', _ => {
+    if(!navIsOpen  ) {
+        responsiveNav.classList.add('open'); //helps to style the toggle lines
+        nav.style.display = 'block'; //reviews the nav
+        navIsOpen = true;
+    } else {
+        
+        responsiveNav.classList.remove('open'); //toggle button returns to normal
+        nav.style.display = 'none';
+        navIsOpen = false;
+    }
+});
 
+// END: Nav Toggle Button
