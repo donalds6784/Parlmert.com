aafa714d3dc3f3617778d290b91a68ea5e020a60
Donalds6784 <donalds6784@gmail.com>
2020-06-01 23:27:57 +0300

    Added refinements to the site

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,13 @@
 'use strict';
 // BEGINNING: Sticky Nav 
-let mainNav = document.querySelector('.nav-header');
 
 window.onscroll = function () {
-    if (window.pageYOffset > 34.545) {
-        mainNav.classList.add('sticky-nav');
-        // mainNav.style.top = 0;
-    } 
-    else {
-        mainNav.classList.remove('sticky-nav');
-    }       
+    let mainNav = document.querySelector('.nav-header');
+    (window.pageYOffset > 34.545) ?
+    mainNav.classList.add('sticky-nav'):
+    mainNav.classList.remove('sticky-nav');       
 };
+
 // END: Sticky nav
 
 // BEGINNING: Nav Toggle Button
